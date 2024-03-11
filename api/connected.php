e825ccb0144064a71fcccf4d152526d05db2bc16
yongmaru <film.shishi123@gmail.com>
2024-03-11 17:32:23 +0700

    addApi

--- api/connected.php
+++ api/connected.php
@@ -4,4 +4,4 @@
     error_reporting(E_ERROR | E_PARSE);
     header("content-type:text/javascript;charset=utf-8");
     $conn = mysqli_connect('localhost', 'root', '', "carpool");
-    // $conn = new mysqli_connect('localhost', 'root', '', "carpool");
+    // $conn = new mysqli_connect('localhost', 'root', '', "svoacar");
